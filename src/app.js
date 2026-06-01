2c7a6c5d311e829c2c5fb7633353eb9c169069de
nathanzerfu <nathancode1214@gmail.com>
2026-06-01 19:21:31 +0300

    task 1-10 finished

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ const trafficMonitor = require('./middlewares/trafficMonitor.middleware');
 
 const app = express();
 
+app.set('trust proxy', 1);
+
 function healthCheck(req, res) {
   res.json({
     status: 'ok',
@@ -31,7 +33,7 @@ const reportsRoutes = require('./modules/reports/reports.routes');
 
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.CORS_ORIGIN || process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
