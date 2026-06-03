f7598788f4bcde775bf57836834c01ac761fd818
nathanzerfu <nathancode1214@gmail.com>
2026-06-03 16:15:18 +0300

    temporary disable csp

--- backend/src/middlewares/helmet.middleware.js
+++ backend/src/middlewares/helmet.middleware.js
@@ -1,26 +1,8 @@
 const helmet = require('helmet');
 
-const frontendUrl =
-  process.env.FRONTEND_URL || 'http://localhost:5173';
-
 const applyHelmet = () => {
   return helmet({
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'"],
-        styleSrc: ["'self'", "'unsafe-inline'"],
-        imgSrc: ["'self'", 'data:', 'blob:'],
-        connectSrc: [
-          "'self'",
-          frontendUrl,
-          'https://charming-vitality-production-a757.up.railway.app'
-        ],
-        fontSrc: ["'self'"],
-        objectSrc: ["'none'"],
-        frameSrc: ["'none'"],
-      },
-    },
+    contentSecurityPolicy: false,
     crossOriginEmbedderPolicy: false,
   });
 };
